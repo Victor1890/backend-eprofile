2febbf496984c610d97912ae8a4f67a4376dba49
Victor J. Rosario V <victorrosario.1890@gmail.com>
2021-05-16 17:57:57 -0400

    [FIX] runtime's dependecies

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 const router = Router();
 
 import * as userCtrl from "../controllers/auth.controller";
-import { verifySignUp } from "../middlewares";
+import { verifySignUp, authJwt } from "../middlewares";
 
 router.post(
   "/signup",
   [verifySignUp.checkDuplicateNickName, verifySignUp.checkDuplicateUserEmail],
   userCtrl.signUp,
 );
+
+router.post("/signin", [authJwt.verifyToken], userCtrl.signIn);
 
 export default router;
